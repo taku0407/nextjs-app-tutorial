d4d55da91d5291653262a65712e994b4f51e7c6a
藤岡拓 <fujioka@fujiokatakunoMacBook-Pro.local>
2025-03-16 11:03:26 +0900

    ESLintエラー対応：不要コード削除

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,9 +1,9 @@
-import bcryptjs from 'bcryptjs';
-import postgres from 'postgres';
+// import bcryptjs from 'bcryptjs';
+// import postgres from 'postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
-import { invoices } from '../lib/placeholder-data';
+// import { invoices } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 // async function seedUsers() {
 //   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -30,31 +30,31 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 //   return insertedUsers;
 // }
 
-async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+// async function seedInvoices() {
+//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      customer_id UUID NOT NULL,
-      amount INT NOT NULL,
-      status VARCHAR(255) NOT NULL,
-      date DATE NOT NULL
-    );
-  `;
+//   await sql`
+//     CREATE TABLE IF NOT EXISTS invoices (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       customer_id UUID NOT NULL,
+//       amount INT NOT NULL,
+//       status VARCHAR(255) NOT NULL,
+//       date DATE NOT NULL
+//     );
+//   `;
 
-  const insertedInvoices = await Promise.all(
-    invoices.map(
-      (invoice) => sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-    ),
-  );
+//   const insertedInvoices = await Promise.all(
+//     invoices.map(
+//       (invoice) => sql`
+//         INSERT INTO invoices (customer_id, amount, status, date)
+//         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+//         ON CONFLICT (id) DO NOTHING;
+//       `,
+//     ),
+//   );
 
-  return insertedInvoices;
-}
+//   return insertedInvoices;
+// }
 
 // async function seedCustomers() {
 //   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -102,17 +102,17 @@ async function seedInvoices() {
 //   return insertedRevenue;
 // }
 
-export async function GET() {
-  try {
-    const result = await sql.begin((sql) => [
-      // seedUsers(),
-      // seedCustomers(),
-      seedInvoices(),
-      // seedRevenue(),
-    ]);
+// export async function GET() {
+//   try {
+//     const result = await sql.begin((sql) => [
+//       // seedUsers(),
+//       // seedCustomers(),
+//       seedInvoices(),
+//       // seedRevenue(),
+//     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json({ message: 'Database seeded successfully' });
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
